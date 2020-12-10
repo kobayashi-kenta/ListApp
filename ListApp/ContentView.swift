b6a75b552e52ff78a93ca9e9d0973d489f71a4dd
kobayashi-kenta <12321klj@gmail.com>
2020-12-10 17:40:02 +0900

    first commit

--- ListApp/ContentView.swift
+++ ListApp/ContentView.swift
@@ -9,8 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("To do リスト")
+                .padding()
+            NavigationView {
+                List(0 ..< 6) { item in
+                    NavigationLink(destination: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Destination@*/Text("Destination")/*@END_MENU_TOKEN@*/) { /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Content@*/Text("Navigate")/*@END_MENU_TOKEN@*/ }
+                }.navigationTitle("今日やること")
+            }
+        }
     }
 }
 
