6f20659582ded0f4c2e6d765e5aa484d1b149288
kobayashi-kenta <12321klj@gmail.com>
2020-12-10 17:10:13 +0900

    Initial Commit

---
+++ ListApp/ListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListAppApp.swift
+//  ListApp
+//
+//  Created by Kenta on 2020/12/10.
+//
+
+import SwiftUI
+
+@main
+struct ListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
